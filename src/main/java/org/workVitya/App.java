50e5ae2b9ed11b5cc6bb0d15b96f78dc8ca2ad98
Viktor <viktorkyndrat@gmail.com>
2022-10-11 19:09:33 +0300

    comitStart

--- src/main/java/org/workVitya/App.java
+++ src/main/java/org/workVitya/App.java
@@ -1,8 +1,14 @@
 package org.workVitya;
 
+import com.google.gson.GsonBuilder;
+
 public class App {
   public static void main(String[] args) {
+    User user = new User();
+    user.setName("Victor");
+    user.setLastName("Kundrat");
+    String json = new GsonBuilder().create().toJson(user);
 
-    System.out.println("Hello World!");
+    System.out.println(json);
   }
 }
---
+++ src/main/java/org/workVitya/User.java
@@ -0,0 +1,22 @@
+package org.workVitya;
+
+public class User {
+    public String name;
+    public String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
